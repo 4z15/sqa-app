41485c13276ee404957708541eff94cbd62f6af4
4z15 <108077746+4z15@users.noreply.github.com>
2024-12-13 14:46:52 +0700

    Update Mahasiswa.php

--- app/Models/Mahasiswa.php
+++ app/Models/Mahasiswa.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Mahasiswa extends Model
 {
@@ -19,4 +20,9 @@ class Mahasiswa extends Model
         'email',
         'poto',
     ];
+
+    public function hitungumur(): int
+    {
+        return Carbon::parse(time : this->tanggal_lahir)->age;
+    }
 }
